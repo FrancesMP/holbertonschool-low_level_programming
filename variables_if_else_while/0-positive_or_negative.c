a9905d62f1e521095b0b13453d03539201101666
Frances <francespalmer@hotmail.fr>
2025-02-19 16:27:06 +0100

    shoutout to trailing white spaces

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -4,26 +4,29 @@
 
 /**
  * main - printing the number stored in variable n
- * need to know if variable is positive or negative
+ * need to know if variable is positive or negative*
  * Return: always 0 (success)
  */
 
 int main(void)
 {
-    int n;
+	int n;
 
-    srand(time(0));
-    n = rand() - RAND_MAX / 2;
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
 
-    if (n > 0) {
-        printf("%d is positive\n", n);
-    }
-    else if (n == 0) {
-        printf("%d is 0\n", n);
-    }
-    else {  // No condition needed here
-        printf("%d is negative\n", n);
-    }
+	if (n > 0)
+	{
+	printf("%d is positive\n", n);
+	}
+	else if (n == 0)
+	{
+	printf("%d is 0\n", n);
+	}
+	else
+	{
+	printf("%d is negative\n", n);
+	}
 
-    return (0);
+	return (0);
 }
