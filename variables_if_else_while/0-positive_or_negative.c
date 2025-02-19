6098cce82cb94265fe857e61cc20c4139067503e
Frances <francespalmer@hotmail.fr>
2025-02-19 15:27:03 +0100

    add program to check if a number is positive, negative, or zero

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -4,24 +4,26 @@
 
 /**
  * main - printing the number stored in variable n
- * need to know if variable is positive or negative*
+ * need to know if variable is positive or negative
  * Return: always 0 (success)
  */
 
 int main(void)
 {
-	int n;
+    int n;
 
-	srand(time(0));
-	n = rand() - RAND_MAX / 2;
-	if (n > 0) { 
-    printf("n is positive\n");
-} 
-else if (n == 0) { 
-    printf("n is 0\n");
-} 
-else { //no condition needed here:
-    printf("n is negative\n");
-}
-	return (0);
+    srand(time(0));
+    n = rand() - RAND_MAX / 2;
+
+    if (n > 0) {
+        printf("%d is positive\n", n);
+    }
+    else if (n == 0) {
+        printf("%d is 0\n", n);
+    }
+    else {  // No condition needed here
+        printf("%d is negative\n", n);
+    }
+
+    return (0);
 }
