532965cf02f0c1332f90b56fdc8b831d1b3dd87d
Frances <francespalmer@hotmail.fr>
2025-02-20 21:15:41 +0100

    printing numbers

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+*main - printing all single digit numbers
+* without using variables of type char*
+*Return: always 0 (success)
+*/
+
+int main(void)
+
+{
+
+	int n;
+
+for (n = 0; n <= 9; n++)
+	putchar(n + '0');
+	putchar(n + '0');
+
+return (0);
+
+}
