d928c26a8971274d0499ebcfee32118a40fa9e57
Frances <francespalmer@hotmail.fr>
2025-02-20 17:24:51 +0100

    printing single digits

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+*main- printing a program that prints all singgle digits
+*They all need to be based on 10 and start from 0*
+*return: always 0 (success)
+*/
+
+int main(void)
+
+{
+
+	int n;
+
+	for (n = '0' ; n <= '9' ; n++)
+	putchar (n);
+
+	putchar('\n');
+
+return (0);
+
+}
