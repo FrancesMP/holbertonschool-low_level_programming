a2e87148f3a7c6d9e23c58b0b41dfae0aa2f9da3
Frances <francespalmer@hotmail.fr>
2025-02-21 16:24:16 +0100

    Print Alpha

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ *print_alphabet - check the code
+ *
+ *Return: Always O
+ */
+void print_alphabet(void)
+{
+	int alpha;
+
+	for (alpha = 97; alpha <= 122; alpha++)
+	{
+		_putchar(alpha);
+	}
+	_putchar('\n');
+}
