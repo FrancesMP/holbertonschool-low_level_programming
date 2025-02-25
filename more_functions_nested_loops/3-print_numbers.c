ac502ec09c87b3e93c74d9ac2fd2651ea88242be
Frances <francespalmer@hotmail.fr>
2025-02-25 19:28:06 +0100

    printing numbers

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints the numbers from 0 to 9
+ */
+void print_numbers(void)
+{
+
+	int nmbr;
+
+	for (nmbr = 48; nmbr < 58; nmbr++)
+
+	{
+		_putchar(nmbr);
+	}
+	_putchar('\n');
+}
