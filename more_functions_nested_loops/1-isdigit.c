32c469dea15fb8951997764294cecd6bfcc04dee
Frances <francespalmer@hotmail.fr>
2025-02-25 14:41:01 +0100

    chechs for digits

---
+++ more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _isdigit - Write function that checks digits
+ * @c: char to check
+ * Return: 1 if c is a digit 0 otherwise
+ */
+
+int _isdigit(int c)
+
+{
+
+	if (c > 47 && c < 58)
+		return (1);
+
+	else
+
+		return (0);
+
+}
